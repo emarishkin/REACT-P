0300b531c2b4393a8045f61a498f65681da3c6b5
eemarishkin <ee.marishkin@yandex.ru>
2024-11-15 23:22:19 +0400

    REACT-P4

--- 01-react-basic-start/src/comonents/introSection.jsx
+++ 01-react-basic-start/src/comonents/introSection.jsx
@@ -1,8 +1,25 @@
+import React from "react";
+
+
+// export function IntroSection(){
+//     // return(
+//     //     <section>
+//     //         <h1 className="centered">Result University</h1>
+//     //         <h3 className="centered" style={{color: '#666'}}>Университет frontend-разработки, который насыщает IT-сферу квалифицированными программистами</h3>
+//     //     </section>
+//     // )
+// }
+
+
+
+
 export function IntroSection(){
-    return(
-        <section>
-            <h1 className="centered">Result University</h1>
-            <h3 className="centered">Университет frontend-разработки, который насыщает IT-сферу квалифицированными программистами</h3>
-        </section>
-    )
+    return (React.createElement('section',
+        null,
+        [React.createElement('h1', {className:"centered"},'Result University'),
+         React.createElement('h3', {className:"centered", style:{color: '#666'}},
+            'Университет frontend-разработки, который насыщает IT-сферу квалифицированными программистами'
+         )
+        ]
+    ))
 }
